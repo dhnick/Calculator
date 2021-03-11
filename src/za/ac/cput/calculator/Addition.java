28ac361331e3dd91a63dc5bbc8e35229eb0f57f5
Damone Hartnick <Damone Hartnick@LAPTOP-QQHESFL8>
2021-03-11 16:20:35 +0200

    Modified the intergerSum() Added doubleSum()

--- src/za/ac/cput/calculator/Addition.java
+++ src/za/ac/cput/calculator/Addition.java
@@ -11,11 +11,35 @@ package za.ac.cput.calculator;
  */
 public class Addition {
     
+      /**
+  This adds two integers
+@param a
+@param b
+@return
+
+
+*/
     
     public static int integerSum( int a , int b)
     {
         return( a+ b);
+     }
+/**
+This adds two real numbers
+@param a
+@param b
+@return 
+
+*/
+
+
+public static double doubleSum( double a , double b  ){
+
+   return a + b;
+
+}
+
     
-    }
+    
     
 }
